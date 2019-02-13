516d843ad1be4f17e00d55e5806751449bada106
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-02-13 10:20:42 +0900

    add homeページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/contact'
+  get 'home/index'
+  get 'home/about'
+  post 'home/afford'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
